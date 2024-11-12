15ef028c5afb9f17fc437466457bdb86a7a6b155
liangxiaojun <fatalxiao@163.com>
2024-11-13 00:13:21 +0800

    update

--- src/drc/drcED50.R
+++ src/drc/drcED50.R
@@ -1,10 +1,10 @@
 # 安装和加载所需的包
 if (!require("drc")) {
-  install.packages("drc")
+    install.packages("drc")
 }
 library(drc)
 
-groupS <- read.csv("./PIEB.csv", 1, encoding='UTF-8')
+groupS <- read.csv("./PIEB.csv", 1, encoding = 'UTF-8')
 groupS
 
 doses <- groupS$doseSequence
@@ -12,8 +12,8 @@ responses <- groupS$responseSequence
 
 # 创建一个数据框
 # data <- data.frame(response = responses, dose = doses)
-data <- data.frame(response = c(rep(1,8),0,1,0,rep(1,5),rep(0,2),rep(1,2),0,1,0,rep(1,3),0,rep(1,7),0,rep(1,3),rep(0,2),rep(1,8),0,1), 
-                   dose = c(rep(25,7),rep(22.5,2),rep(25,2),rep(27.5,6),30,rep(32.5,3),rep(35,2),rep(37.5,4),rep(40,5),rep(37.5,3),rep(40,4),42.5,rep(45,9),47.5))
+data <- data.frame(response = c(rep(1, 8), 0, 1, 0, rep(1, 5), rep(0, 2), rep(1, 2), 0, 1, 0, rep(1, 3), 0, rep(1, 7), 0, rep(1, 3), rep(0, 2), rep(1, 8), 0, 1),
+                   dose = c(rep(25, 7), rep(22.5, 2), rep(25, 2), rep(27.5, 6), 30, rep(32.5, 3), rep(35, 2), rep(37.5, 4), rep(40, 5), rep(37.5, 3), rep(40, 4), 42.5, rep(45, 9), 47.5))
 data
 
 # 使用drc包的drm()函数拟合剂量-反应模型
@@ -28,5 +28,3 @@ summary(model)
 # 第一个参数是模型，第二个参数是你想要计算的药效比例（在这里是50）
 ed90 <- ED(model, 90, interval = "delta")
 ed90
-
-
