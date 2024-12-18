9b442d4f9ee104250e515be923d5f5369914b82d
liangxiaojun <fatalxiao@163.com>
2024-12-18 13:07:39 +0800

    update

--- src/upndown/udplot.R
+++ src/upndown/udplot.R
@@ -11,10 +11,15 @@ library(upndown)
 
 # It was a classical median-finding up-and-down study.
 
-doses <- c(4:7, 6:13, 12:19, 18:21, 20, 19:23, 22, 21:23, 22:19, 20:23,
-           22:24, 23, 22, 23, 22:25, 24:22, rep(23:24, 2), 23, 22)
-# With U&D, responses (except the last one) can be read off the doses:
-responses <- c((1 - sign(diff(doses))) / 2, 0)
+# doses <- c(4:7, 6:13, 12:19, 18:21, 20, 19:23, 22, 21:23, 22:19, 20:23,
+#            22:24, 23, 22, 23, 22:25, 24:22, rep(23:24, 2), 23, 22)
+# # With U&D, responses (except the last one) can be read off the doses:
+# responses <- c((1 - sign(diff(doses))) / 2, 0)
+
+groupS <- read.csv("./loading volume data.csv", 1, encoding = 'UTF-8')
+
+doses <- groupS$doseSequence
+responses <- groupS$responseSequence
 
 
 #' ### Plots plots plots!
@@ -28,22 +33,22 @@ par(mar = c(4, 4, 4, 1), mgp = c(2.5, 0.8, 0), cex.axis = 0.7, las = 1)
 #' The experimental trajectory / time-series / "trace" (pick your favorite name!)
 #' Note the changed argument names for x and y axis titles
 udplot(doses, responses, main = '',
-       xtitle = "Patient Number", ytitle = 'Gabapentin (mg/kg)')
+xtitle = "Patient Number", ytitle = 'Gabapentin (mg/kg)')
 #' Compare with the article's Figure 1; the line below makes it look more similar
 udplot(doses, responses, shape = 'square', connect = TRUE)
 
 # The dose-response plot, rarely encountered in U&D articles. 
 # We can also add the CIR estimate right there:
 drplot(doses, responses, main = ' Dose-Response', percents = TRUE,
-       addest = TRUE, target = 0.5, addcurve = TRUE,
-       xtitle = 'Gabapentin (mg/kg)', ytitle = "Percent Effective")
+addest = TRUE, target = 0.9, addcurve = TRUE,
+xtitle = 'Gabapentin (mg/kg)', ytitle = "Percent Effective")
 
 #' ### Estimates
 
 #' Let us actually see the numbers of those Centered-Isotonic-Regression (CIR) estimates!
 #' Note that our default confidence-interval is 90\%. Change it via the 'conf' argument.
 
-udest(doses, responses, target = 0.5)
+udest(doses, responses, target = 0.9)
 #' Compare with the article: 21.7 mg/kg (95\% CI 19.9–23.5). 
 #' They cite a little-known 1991 article by Dixon as the method source.
 #' However, in their author rejoinder they claim to have used the Dixon-Mood estimate.
